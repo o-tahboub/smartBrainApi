1b0d9d3868b511b505ff415e971f522f746d3288
o-tahboub <o.tahboob2000@gmail.com>
2023-09-21 19:10:59 +0100

    Moved database details to environment varibles

--- server.js
+++ server.js
@@ -23,11 +23,11 @@ const port = 3000
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      port : 5432,
-      user : 'postgres',
-      password : '',
-      database : 'smart-brain'
+      host : process.env.DATABASE_HOST,
+      port : process.env.DATABASE_PORT,
+      user : process.env.DATABASE_USER,
+      password : process.env.DATABASE_PASSWORD,
+      database : process.env.DATABASE_NAME
     }
   });
 
